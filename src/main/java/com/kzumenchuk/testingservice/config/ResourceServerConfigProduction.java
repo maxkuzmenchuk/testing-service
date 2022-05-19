acf139b63b7f20f579ab1621d15016a0b8cda2c0
Maxim Kuzmenchuk <46691880+maxkuzmenchuk@users.noreply.github.com>
2022-05-19 16:57:08 +0300

    added spring profiles

--- src/main/java/com/kzumenchuk/testingservice/config/ResourceServerConfigProduction.java
+++ src/main/java/com/kzumenchuk/testingservice/config/ResourceServerConfigProduction.java
@@ -1,11 +1,13 @@
 package com.kzumenchuk.testingservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+@Profile("prod")
+public class ResourceServerConfigProduction extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
